3d75026ff05f177bbf50eb23f41b551b22e4c9dc
emarekica <emarekica@gmail.com>
2021-11-16 21:11:41 +0100

    mahe clean JS file

--- wall-chat.js
+++ wall-chat.js
@@ -1,8 +1,12 @@
 // "use strict";
 
-const CLIENT_ID = 'M4trM8H1WVeEhszi';
+// Connecting to Scaledrone channel ID
+const CLIENT_ID = "M4trM8H1WVeEhszi";
 
+// creating instance for a single connection
 const drone = new ScaleDrone(CLIENT_ID, {
+  // adding custom data to members (set as JS object)
+  // properties sent out as clientData via events/passed to each user
   data: {
     name: getRandomName(),
     color: getRandomColor(),
@@ -12,62 +16,63 @@ const drone = new ScaleDrone(CLIENT_ID, {
 // Stores members
 let members = [];
 
-
-// connecting to and joining the room
-drone.on('open', error => {
+//// ----------------------------- OPENING CONNECTION
+// events: open, error
+// a connection has been opened, if no errors
+drone.on("open", (error) => {
   if (error) {
     return console.error(error);
   }
-  console.log('Successfully connected to Scaledrone');
+  console.log("Successfully connected to Scaledrone");
 
   // Subscribe to messages
   // wait until it is successful and then execute the first passed callback function - updateMembersDOM()
-  const room = drone.subscribe('observable-room');
+  const room = drone.subscribe("observable-room");
 
-  room.on('open', error => {
+  room.on("open", (error) => {
     if (error) {
       return console.error(error);
     }
-    console.log('Successfully joined room');
+    console.log("Successfully joined room");
   });
 
   // Gives array of members that joined the room
-  room.on('members', m => {
+  room.on("members", (m) => {
     members = m;
     updateMembersDOM();
   });
 
   // New member joins the room
-  room.on('member_join', member => {
+  room.on("member_join", (member) => {
     members.push(member);
     updateMembersDOM();
   });
 
   // Member leaves the room
-  room.on('member_leave', ({id}) => {                              // wtf on
-    const index = members.findIndex(member => member.id === id);   
+  room.on("member_leave", ({ id }) => {
+    // wtf on
+    const index = members.findIndex((member) => member.id === id);
     members.splice(index, 1);
     updateMembersDOM();
   });
 
   // Listen to messages sent by users & add them to messages <div>
-  room.on('data', (text, member) => {
+  room.on("data", (text, member) => {
     if (member) {
       addMessageToListDOM(text, member);
-    } 
+    }
   });
 });
 
 // Closing connection to Scaledrone
-drone.on('close', event => {
-  console.log('Connection was closed', event);
+drone.on("close", (event) => {
+  console.log("Connection was closed", event);
 });
 
 // Problems with the connection
-drone.on('error', error => {
+drone.on("error", (error) => {
   console.error(error);
 });
-
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -107,7 +112,7 @@ function getRandomName() {
     "ramus",
     "sabell",
     "sentis",
-    "sisymbrium",   
+    "sisymbrium",
   ];
   return (
     adjs[Math.floor(Math.random() * adjs.length)] +
@@ -116,48 +121,45 @@ function getRandomName() {
   );
 }
 
-
-// Function to get random color 
+// Function to get random color
 function getRandomColor() {
   return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
 }
-
 
 ///////////////////////////////////////////////////////
 
 // DOM related
 
 const DOM = {
-  membersCount: document.querySelector('.members-count'),
-  membersList: document.querySelector('.members-list'),
-  messages: document.querySelector('.messages'),
-  input: document.querySelector('.message-form__input'),
-  form: document.querySelector('.message-form'),
+  membersCount: document.querySelector(".members-count"),
+  membersList: document.querySelector(".members-list"),
+  messages: document.querySelector(".messages"),
+  input: document.querySelector(".message-form__input"),
+  form: document.querySelector(".message-form"),
 };
 
 // Event listener for sending messages
-DOM.form.addEventListener('submit', sendMessage);
+DOM.form.addEventListener("submit", sendMessage);
 
 function sendMessage() {
   const value = DOM.input.value;
-  if (value === '') {
+  if (value === "") {
     return;
   }
-  DOM.input.value = '';                 // how to write differently, publish() is deprecated
-  drone.publish({                       
-    room: 'observable-room',
+  DOM.input.value = ""; // how to write differently, publish() is deprecated
+  drone.publish({
+    room: "observable-room",
     message: value,
   });
 }
 
-
 // creating and adding MEMBERS
 function createMemberElement(member) {
-  const { name, color } = member.clientData;          
+  const { name, color } = member.clientData;
 
-  const el = document.createElement('div');
+  const el = document.createElement("div");
   el.appendChild(document.createTextNode(name));
-  el.className = 'member';
+  el.className = "member";
   el.style.color = color;
 
   return el;
@@ -166,19 +168,19 @@ function createMemberElement(member) {
 // Who's online
 function updateMembersDOM() {
   DOM.membersCount.innerText = `${members.length} users in room:`;
-  DOM.membersList.innerHTML = '';
+  DOM.membersList.innerHTML = "";
 
-  members.forEach(member =>
+  members.forEach((member) =>
     DOM.membersList.appendChild(createMemberElement(member))
   );
 }
 
 // creating and adding MESSAGES
 function createMessageElement(text, member) {
-  const el = document.createElement('div');
+  const el = document.createElement("div");
   el.appendChild(createMemberElement(member));
   el.appendChild(document.createTextNode(text));
-  el.className = 'message';
+  el.className = "message";
 
   return el;
 }
